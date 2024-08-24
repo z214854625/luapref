770be8ea33dbfbb60da84eea9b5b836ccac88369
chencaiyu <1970326764@qq.com>
2024-08-24 17:47:53 +0800

    增加是否开启采样模式(默认采样策略)

--- src/xLuaProfiler.cpp
+++ src/xLuaProfiler.cpp
@@ -13,7 +13,8 @@
 sampling type : collect the function call times by sampling
 tick type : get the all call stacks by this tick
 */
-#define sampling
+//#define sampling
+bool enable_sample = true;
 
 int grunning = 0;
 bool openHook = true;
@@ -25,29 +26,29 @@ extern "C" int lrealstop(lua_State *L)
   lua_sethook(L, 0, 0, 0);
   grunning = 0;
 
-  if (curTraceTree)
-  {
+  if (curTraceTree) {
     curTraceTree->mergeAllNodes();
     TSadPtrNode root = curTraceTree->getspRoot();
-    #ifndef sampling 
-    curTraceTree->printTree(root, 0);
-    #else
-    curTraceTree->printfolded(root, "");
-    #endif
+    if (!enable_sample) {
+      curTraceTree->printTree(root, 0);
+    }
+    else {
+      curTraceTree->printfolded(root, "");
+    }
   }
 
-#ifdef sampling
-  struct itimerval timer;
-  timer.it_interval.tv_sec = 0;
-  timer.it_interval.tv_usec = 0;
-  timer.it_value = timer.it_interval;
-  int ret = setitimer(ITIMER_PROF, &timer, NULL);
-  if (ret != 0)
-  {
-    PERR("setitimer fail %d", ret);
-    return ret;
+  if (enable_sample) {
+    struct itimerval timer;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 0;
+    timer.it_value = timer.it_interval;
+    int ret = setitimer(ITIMER_PROF, &timer, NULL);
+    if (ret != 0)
+    {
+      PERR("setitimer fail %d", ret);
+      return ret;
+    }
   }
-#endif
 
   return 0;
 }
@@ -105,8 +106,8 @@ extern "C" int lrealstart(lua_State *L, int frequencyPerSecond)
   }
   grunning = 1;
   gL = L;
-  signal(SIGINT, sig_handler);
-  signal(SIGSEGV, sig_handler);
+  // signal(SIGINT, sig_handler);
+  // signal(SIGSEGV, sig_handler);
 
   curTraceTree = std::shared_ptr<TraceInfoTree>(new TraceInfoTree());
   if (curTraceTree)
@@ -114,37 +115,38 @@ extern "C" int lrealstart(lua_State *L, int frequencyPerSecond)
     curTraceTree->init();
   }
 
-#ifdef sampling
-  struct sigaction sa;
-  sa.sa_sigaction = SignalHandler;
-  sa.sa_flags = SA_RESTART | SA_SIGINFO;
-  sigemptyset(&sa.sa_mask);
+  if (enable_sample) {
+    struct sigaction sa;
+    sa.sa_sigaction = SignalHandler;
+    sa.sa_flags = SA_RESTART | SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
 
-  if (sigaction(SIGPROF, &sa, NULL) == -1)
-  {
-    PERR("sigaction(SIGALRM) failed");
-    return -1;
+    if (sigaction(SIGPROF, &sa, NULL) == -1)
+    {
+      PERR("sigaction(SIGALRM) failed");
+      return -1;
+    }
+
+    // default sampling frequecy is 999Hz
+    frequencyPerSecond = frequencyPerSecond < 0 ? -frequencyPerSecond : frequencyPerSecond;
+    unsigned int frequency = frequencyPerSecond == 0 ? 999 : frequencyPerSecond;
+
+    struct itimerval timer;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 1000000 / frequency;
+    timer.it_value = timer.it_interval;
+    int ret = setitimer(ITIMER_PROF, &timer, NULL);
+    if (ret != 0)
+    {
+      PERR("setitimer fail %d", ret);
+      return -1;
+    }
+
+    // PDBG("lrealstart frequency:%u %s", frequency, file);
   }
-
-  // default sampling frequecy is 999Hz
-  frequencyPerSecond = frequencyPerSecond < 0 ? -frequencyPerSecond : frequencyPerSecond;
-  unsigned int frequency = frequencyPerSecond == 0 ? 999 : frequencyPerSecond;
-
-  struct itimerval timer;
-  timer.it_interval.tv_sec = 0;
-  timer.it_interval.tv_usec = 1000000 / frequency;
-  timer.it_value = timer.it_interval;
-  int ret = setitimer(ITIMER_PROF, &timer, NULL);
-  if (ret != 0)
-  {
-    PERR("setitimer fail %d", ret);
-    return -1;
+  else{
+    lua_sethook(gL, SignalHandlerHookSwitch, LUA_MASKCALL | LUA_MASKRET, 0);
   }
-
-  // PDBG("lrealstart frequency:%u %s", frequency, file);
-#else
-  lua_sethook(gL, SignalHandlerHookSwitch, LUA_MASKCALL | LUA_MASKRET, 0);
-#endif
   return 0;
 }
 
@@ -165,12 +167,20 @@ static int lstop(lua_State *L)
   return 1;
 }
 
+static int lenablesample(lua_State *L)
+{
+  int usesample = (int)lua_tointeger(L, 1);
+  enable_sample = (usesample == 1);
+  return 1;
+}
+
 extern "C" int luaopen_libluaperf(lua_State *L)
 {
   luaL_checkversion(L);
   luaL_Reg l[] = {
       {"start", lstart},
       {"stop", lstop},
+      {"enablesample", lenablesample},
       {NULL, NULL},
   };
   luaL_newlib(L, l);
